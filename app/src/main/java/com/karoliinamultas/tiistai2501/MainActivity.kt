7cc2f8630e201886beff037e9111b1a67ee3ae52
karoliam <kmultas@gmail.com>
2022-01-27 22:01:17 +0200

    fixed everything

--- app/src/main/java/com/karoliinamultas/tiistai2501/MainActivity.kt
+++ app/src/main/java/com/karoliinamultas/tiistai2501/MainActivity.kt
@@ -4,40 +4,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
+import com.karoliinamultas.tiistai2501.MemberOfParliament.ParliamentMembersData.members
 import com.karoliinamultas.tiistai2501.databinding.ActivityMainBinding
+import kotlin.math.log
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var on = false
-    val parties = MemberOfParliament.ParliamentMembersData.members.map { it.party }.toSet().sorted()
+    val parties = members.map { it.party }.toSet().sorted()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        var member = MemberOfParliament.ParliamentMembersData.members
-
-/*        var memberMap: MutableMap<Int, String> = mutableMapOf()
-        for(i in 0..member.size.minus(1)){
-            memberMap.put(member[i].personNumber, member[i].party)
-        }*/
-
-
         binding.partiesText.text = parties.toString()
 
-        binding.button.setOnClickListener {
-                findRandomPolitician(it)
+        fun findRandomPolitician(view: View) {
+
+            val input = binding.userInput.text.toString()
+            if (parties.any { it.equals(input)}) {
+
+                val allMembersFromSameParty = members.filter { member ->
+                    member.party.contains(input)
+                }
+                val randMember = allMembersFromSameParty.random()
+
+                binding.politicianName.text = randMember.first + " " + randMember.last + " " + randMember.party
+
             }
-
+            else binding.politicianName.text = "Try again."
+        }
+        binding.button.setOnClickListener {
+            findRandomPolitician(it)
         }
 
-    fun findRandomPolitician(viewThatIsClicked: View) {
-        val input = binding.politicianName.text
-        if((parties.any{ it.equals(input) })){
-            val politician = MemberOfParliament.ParliamentMembersData.members.map { it.last }.toSet().sorted().toString()
-            val randomPolitician = politician.random()
-            binding.politicianName.text = randomPolitician.toString()
-        }
     }
 
-    }
+}
+
+
